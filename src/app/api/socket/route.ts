79220cfe90163276c953225d3df5a0ed37bd926e
JanAyann <act.jiencabo@gmail.com>
2026-05-28 12:46:18 +0800

    Update route.ts

--- src/app/api/socket/route.ts
+++ src/app/api/socket/route.ts
@@ -1,13 +1,6 @@
-import { NextApiRequest } from 'next'
-import { Server as HTTPServer } from 'http'
-import { Server as SocketIOServer } from 'socket.io'
-import { initSocket, NextApiResponseWithSocket } from '@/lib/socket'
+import { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export default function handler(req: NextApiRequest, res: any) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' })
-  }
-
-  const io = initSocket(res as NextApiResponseWithSocket)
-  res.end()
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: 'Socket.IO server not available in App Router' })
 }
